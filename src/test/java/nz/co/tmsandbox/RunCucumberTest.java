ce217fbed40d8a7f8b9145204fe753209efb05e6
Dilinika Karunanayaka <48257667+Dilinika-Karunanayaka@users.noreply.github.com>
2024-06-11 14:09:47 +1200

    Add assertions

--- src/test/java/nz/co/tmsandbox/RunCucumberTest.java
+++ src/test/java/nz/co/tmsandbox/RunCucumberTest.java
@@ -5,6 +5,7 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
@@ -16,5 +17,9 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
                 "json:target/cucumber-reports/Cucumber.json," +
                 "html:target/cucumber-reports/Cucumber.html," +
                 "timeline:target/cucumber-reports/CucumberTimeline")
+/*@ConfigurationParameter(
+        key = FILTER_TAGS_PROPERTY_NAME,
+        value = "@WIP"
+)*/
 public class RunCucumberTest {
 }
